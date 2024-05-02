9990bc504b5736e4e6f5bf1a9e438297601f783a
Valentin Morişcă <516431@student.fontys.nl>
2024-05-03 01:49:12 +0200

    Initial commit

--- src/components/EventComponent.jsx
+++ src/components/EventComponent.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react"; 
 import { Link } from 'react-router-dom';
-import '../css/EventPostStyle.css';
 
 const EventComponent = () => {
     const [formData, setFormData] = useState({
